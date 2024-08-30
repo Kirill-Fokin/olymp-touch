895728008e1f4dd3235c0bcc0dc5e438087ac664
Kirill-Fokin <kirill19011998@mail.ru>
2024-08-30 19:34:20 +0300

    feat: add layout

--- index.js
+++ index.js
@@ -1,44 +1,32 @@
-// slider
 const slider = document.querySelector('.slider__wrapper');
-
-slider.style.width = window.innerWidth;
 
 const sliderImgs = Array.from(document.querySelectorAll('.slide__image'));
 
 const slideWidth = sliderImgs[0].offsetWidth;
+
+
+
+function slideBack() {
+  offset -= 2;
+}
+
+
+
 
 let offset = 1;
 
 const imgSrc = sliderImgs.reduce((sum, cur) => {
   sum.push(cur.src);
   cur.remove()
-return sum; 
+
+  return sum; 
 }, []);
-
-
-const getvalue = (tag, prop) => {
-  const elem = document.querySelector(tag);
-  console.log(elem)
-  const styles = window.getComputedStyle(elem)
-
-  return styles[prop].slice(0, -2);
-}
 
 const getCurrentWindowWidth = () => window.innerWidth();
 
 function draw() {
   const windowWidth = window.innerWidth;
   console.log(windowWidth, slideWidth)
-
-
-  const firstCoordinates = 0;
-  const secondCoordinates = slideWidth + 1;
-
-  const thirdCoordinates = secondCoordinates + slideWidth;
-
-
-
-  let offset = 0;
   for (let i = 0; i <  3; i++) {
     const img = document.createElement('img');
     img.classList.add('slide__image');
@@ -59,19 +47,16 @@ function move() {
   offset++;
 }
 
-
 setTimeout(() => {
-  move()
+  const image = document.createElement('img');
+  image.src = imgSrc[0];
+  image.classList.add('slide__image')
+  slider.append(image)
+  slider.style.right =  (+slideWidth / 2 * offset)  + 'px'
+  offset++;
 }, 1000)
 
-
-
-
-
-
-
-
-
-
-
-
+// let timerId = setTimeout(function tick() {
+//   move()
+//   timerId = setTimeout(tick, 2000); // (*)
+// }, 2000);
